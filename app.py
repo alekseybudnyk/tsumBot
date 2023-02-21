50161068e0d6a5d187687176eabe6159bf2ae652
oleksii.budnyk <budnykoleksii@gmail.com>
2023-02-21 22:50:48 +0200

    easy2

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
 
-import os
-import requests
-from requests.auth import HTTPBasicAuth
+
 
 bot = Bot(token='5807710356:AAENK0tO3zvdzqgK54vHhRrOineIYr8oX2o')
 dp = Dispatcher(bot)
