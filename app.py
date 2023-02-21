f4c6556a6e9761f9462cc5bf6050f8773d7535e3
oleksii.budnyk <budnykoleksii@gmail.com>
2023-02-21 20:43:40 +0200

    123

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print('Hello')
+import os
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, I'm a bot!")
+
+def main():
+    token = "5807710356:AAENK0tO3zvdzqgK54vHhRrOineIYr8oX2o"
+    updater = Updater(token, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
